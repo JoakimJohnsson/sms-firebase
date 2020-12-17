c4cb36f3d93249135233c7b82e8f9bace6ad44e0
Joakim Johnsson <you@example.com>
2020-12-17 05:48:47 +0100

    new files

---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../assets/scss/main.scss';
+
+const App = () => (
+    <div>
+        <h1 className="btn btn-info">App</h1>
+        <h1 className="btn btn-info">App</h1>
+        <h1 className="btn btn-info">App</h1>
+        <h1 className="btn btn-info">App</h1>
+
+    </div>
+);
+
+export default App;
