676d3e8d626a1876ac3aa67287af0ef74daed8a9
JoakimJohnsson <joakim.johnsson@su.se>
2020-12-20 20:30:36 +0100

    add to be sign in and sign up modals

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,9 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Button, Modal} from "react-bootstrap";
 
-const Landing = () => (
-    <div>
-        <h1>Svenska marvelsamlare</h1>
-    </div>
-);
+
+function Landing() {
+
+    const [show, setShow] = useState(false);
+    const [show2, setShow2] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    const handleClose2 = () => setShow2(false);
+    const handleShow2 = () => setShow2(true);
+
+    return (
+        <div>
+            <h1>Svenska marvelsamlare</h1>
+
+            <>
+                <Button variant="primary" onClick={handleShow}>
+                    Launch demo modal
+                </Button>
+
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={handleClose}>
+                            Save Changes
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+
+                <Button variant="primary" onClick={handleShow2}>
+                    Launch asdfasdf demo modal
+                </Button>
+
+                <Modal show={show2} onHide={handleClose2}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>asdfasdfasd Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose2}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={handleClose2}>
+                            Save Changes
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </>
+
+
+        </div>
+    );
+}
 
 export default Landing;
