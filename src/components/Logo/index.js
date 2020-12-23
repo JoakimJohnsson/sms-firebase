87ad2aa2a11e934c27020857195717742a161dde
JoakimJohnsson <joakim.johnsson@su.se>
2020-12-23 20:46:20 +0100

    improvements

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -9,7 +9,8 @@ const SmsLogo = (props) => (
     <div className={"sms-logo navbar-brand " + (props.showWhite ? "text-white" : "text-black")}>
         <Link to={ROUTES.START} className={"d-flex align-items-center"}>
             <img className="sms-logo__image me-2" src={props.showWhite ? shieldWhite : shieldBlack} alt={"Svenska Marvelsamlare Logo"} />
-                <span className={"sms-logo__text"}>SVENSKA MARVELSAMLARE</span>
+                <span className={"sms-logo__text d-none d-sm-inline"}>SVENSKA MARVELSAMLARE</span>
+                <span className={"sms-logo__text d-sm-none"}>SMS</span>
         </Link>
     </div>
 );
