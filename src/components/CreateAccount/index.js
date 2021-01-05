8786807f27421afbc18aa48bb2762e39738b1222
JoakimJohnsson <joakim.johnsson@su.se>
2021-01-05 15:54:36 +0100

    minor fix

--- src/components/CreateAccount/index.js
+++ src/components/CreateAccount/index.js
@@ -14,8 +14,8 @@ const CreateAccountPage = () => (
             </div>
             <div className="container-fluid">
             <div className="row mb-5">
-                <div className="col-12 col-lg-8 mb-5 mb-md-0">
-                    <div className="me-0 me-md-3 sms-form-box__neu">
+                <div className="col-12 col-lg-8 mb-5 mb-lg-0">
+                    <div className="me-0 me-lg-3 sms-form-box__neu">
                         <h2 className="form-header">Sign up</h2>
                         <SignUpForm/>
                     </div>
