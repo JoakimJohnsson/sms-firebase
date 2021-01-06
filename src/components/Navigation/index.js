379ea0047e202cc2ab4aabdf834e193398df4121
JoakimJohnsson <joakim.johnsson@su.se>
2021-01-06 19:53:34 +0100

    changes i18n layouts

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -56,13 +56,7 @@ const HeaderAuth = ({authUser}) => {
                             <NavLinkComponentWithIcon link={ROUTES.ADMIN} text={"Administration"} icon={<Icon.Bug className="fs-5 me-1"/>}
                                                       setExpanded={setExpanded}/>
                         )}
-                        <button aria-label={t('btn_txt_change_language')}
-                                className="btn btn-secondary ms-lg-3"
-                                title={t('btn_txt_change_language')}
-                                onClick={changeLanguage()}>
-                            <Icon.Globe2 className="fs-4 me-2 me-lg-0"/>
-                            <span className="d-lg-none">{t('btn_txt_change_language')}</span>
-                        </button>
+                        <ChangeLanguageButton t={t}/>
                         <SignOutButton/>
                     </Nav>
                 </Navbar.Collapse>
@@ -86,13 +80,7 @@ const HeaderNonAuth = ({onTop}) => {
                     <Nav className="mr-auto">
                         <NavLinkComponent link={ROUTES.START} text={"Start"} isExact={true} setExpanded={setExpanded}/>
                         <NavLinkComponent link={ROUTES.CREATE_ACCOUNT} text={"Create account"} setExpanded={setExpanded}/>
-                        <button aria-label={t('btn_txt_change_language')}
-                                className="btn btn-secondary ms-lg-3"
-                                title={t('btn_txt_change_language')}
-                                onClick={changeLanguage()}>
-                            <Icon.Globe2 className="fs-4 me-2 me-lg-0"/>
-                            <span className="d-lg-none">{t('btn_txt_change_language')}</span>
-                        </button>
+                        <ChangeLanguageButton t={t}/>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
@@ -110,6 +98,17 @@ const changeLanguage = () => {
         console.log("current lang " + i18n.language)
     }
 }
+
+const ChangeLanguageButton = ({t}) => (
+    <button aria-label={t('btn_txt_change_language')}
+            className="btn btn-secondary ms-lg-3 d-flex align-items-center justify-content-center"
+            title={t('btn_txt_change_language')}
+            onClick={changeLanguage()}>
+        <Icon.Globe2 className="fs-4 me-2 me-lg-0 me-xl-2"/>
+        <span className="d-inline d-lg-none">{t('btn_txt_change_language')}</span>
+        <span className="d-none d-xl-inline">{t('btn_txt_change_language_short')}</span>
+    </button>
+);
 
 const NavLinkComponent = ({link, text, isExact, setExpanded}) => (
     <NavLink className="nav-link" to={link} exact={isExact} onClick={() => setExpanded(false)}>{text}</NavLink>
--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -5,8 +5,8 @@ import * as Icon from "react-bootstrap-icons";
 
 const SignOutButton = ({ firebase }) => (
     <button className="btn btn-primary ms-lg-3" type="button" onClick={firebase.doSignOut} title="Sign out">
-        <Icon.ShieldLock className="fs-4 me-2 me-lg-0"/>
-        <span className="d-lg-none">Sign Out</span>
+        <Icon.ShieldLock className="fs-4 me-2 me-lg-0 me-xl-2"/>
+        <span className="d-inline d-lg-none d-xl-inline">Sign Out</span>
     </button>
 );
 
